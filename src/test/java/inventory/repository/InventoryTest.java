441178e63c8f535e79c418aab25f71f2e68c3f3f
Mitro <mitro.andrei@gmail.com>
2024-04-06 18:23:18 +0300

    Lab03-finished

---
+++ src/test/java/inventory/repository/InventoryTest.java
@@ -0,0 +1,83 @@
+package inventory.repository;
+
+import inventory.errors.RepositoryException;
+import inventory.errors.ValidationException;
+import inventory.model.InhousePart;
+import inventory.model.Part;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventoryTest {
+
+    private Inventory inventory;
+    private final String searchItemValid = "piesa_test";
+
+    private final Part partToFind = new InhousePart(100, searchItemValid, 10.0, 10, 1, 100, 1);
+
+    @BeforeEach
+    void setUp() {
+        inventory = new Inventory();
+    }
+
+    @Test
+    void lookupPartValid() {
+        generateParts(1);
+        inventory.addPart(partToFind);
+        List<Part> part = inventory.lookupParts(searchItemValid);
+        assertNotNull(part, "Part not found");
+        assertNotEquals(0, part.size(), "Part not found");
+        assertEquals(1, part.size(), "More than one part found");
+        assertEquals(partToFind, part.get(0), "Part not found");
+    }
+
+    @Test
+    void lookupPartNonexistent() {
+        generateParts(1);
+        String searchItemNonexistent = "non_existent";
+        try {
+            inventory.lookupParts(searchItemNonexistent);
+            fail("Part found");
+        } catch (RepositoryException e){
+            assertEquals("No parts found", e.getMessage(), "Wrong exception message");
+        }
+    }
+
+    @Test
+    void lookupPartEmpty() {
+        String searchItemNoParts = "no_parts";
+        try {
+            inventory.lookupParts(searchItemNoParts);
+            fail("Part found");
+        } catch (RepositoryException e){
+            assertEquals("No parts found", e.getMessage(), "Wrong exception message");
+        }
+    }
+
+    @Test
+    void lookupPartEmptyString() {
+        String searchItemEmpty = "";
+        try {
+            inventory.lookupParts(searchItemEmpty);
+            fail("Part found");
+        } catch (ValidationException e){
+            assertEquals("Search string is empty", e.getMessage(), "Wrong exception message");
+        }
+    }
+
+    void generateParts(int n){
+        for(int i=0; i<n; i++){
+            inventory.addPart(new InhousePart(i, "Piesa"+i, 10.0, 10, 1, 100, i));
+        }
+    }
+
+
+    @AfterEach
+    void tearDown() {
+        inventory = null;
+    }
+}
